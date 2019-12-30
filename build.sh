4aee7341713949e628d82462f8beda1a70d3eb70
Andrew Tan <andrewvmail@gmail.com>
2019-12-29 22:37:24 -0800

    commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo build
+
+wget http://lua.sqlite.org/index.cgi/zip/lsqlite3_fsl09y.zip
+unzip lsqlite3_fsl09y.zip
+cd lsqlite3_fsl09y/
+luarocks make ./lsqlite3-0.9.5-1.rockspec SQLITE_DIR=/usr/local/opt/sqlite3/
+cp -r ../lsqlite3.c.patch .
+patch < lsqlite3.c.patch 
