91270c077dd52a198f6cbc759f15c3c8ef5320dc
Andrew Tan <andrewvmail@gmail.com>
2019-12-29 22:40:04 -0800

    oops

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ echo build
 
 wget http://lua.sqlite.org/index.cgi/zip/lsqlite3_fsl09y.zip
 unzip lsqlite3_fsl09y.zip
+
+
 cd lsqlite3_fsl09y/
-luarocks make ./lsqlite3-0.9.5-1.rockspec SQLITE_DIR=/usr/local/opt/sqlite3/
 cp -r ../lsqlite3.c.patch .
 patch < lsqlite3.c.patch 
+luarocks make ./lsqlite3-0.9.5-1.rockspec SQLITE_DIR=/usr/local/opt/sqlite3/
